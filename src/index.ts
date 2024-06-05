36a1c3c31d35df3e1cedfdb65cfe608d4ea40af2
afedotov <fedotov_as@ascon.ru>
2024-06-06 00:29:28 +0400

    init commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './index.css';
+
+
+function main(): void {
+  console.log('Hello');
+}
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'web components basic typescript', 
+      template: 'src/index.html' }),
+    new MiniCssExtractPlugin({filename:"bundle.css"})
+   ],
+};
